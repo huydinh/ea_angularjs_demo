106e8599d0bb611072508dcf42d907426bbafd12
East Agile 10.9 <developers@eastagile.com>
2014-02-14 14:56:25 +0700

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EaAngularjsDemo::Application.config.secret_key_base = '498192eb60062453d536f232644fbdd9c82d57e790945609484917896daf3781e07b7fe7f21938f4ccceaab49e081fb449f88190a79c0c98a1ed44de799f96f0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EaAngularjsDemo::Application.config.session_store :cookie_store, key: '_ea_angularjs_demo_session'
